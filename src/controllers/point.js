4ffc84b81439beaa1e8c46e8d6c77d0c81c0225e
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-29 11:30:16 +0300

    6.1.1 Создаст контроллер точки маршрута

---
+++ src/controllers/point.js
@@ -0,0 +1,13 @@
+import EventEditComponent from "../components/event-edit.js";
+import EventComponent from "../components/event.js";
+import {RenderPosition, render, replace} from "../utils/render.js";
+
+export default class PointController {
+  constructor(container) {
+
+  }
+
+  render() {
+
+  }
+}
