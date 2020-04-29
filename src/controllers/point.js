26161d2c18d375e72f09eed938582243e905cf89
Leprechaon <38357060+Leprechaon@users.noreply.github.com>
2020-04-29 12:17:00 +0300

    6.1.2 Перенесёт в контроллер точки маршрута логику переключения. WIP

--- src/controllers/point.js
+++ src/controllers/point.js
@@ -1,13 +1,81 @@
+import EventDetailsComponent from "../components/event-details.js";
 import EventEditComponent from "../components/event-edit.js";
 import EventComponent from "../components/event.js";
 import {RenderPosition, render, replace} from "../utils/render.js";
+import EventOffersComponent from "../components/event-offers.js";
+import EventDestinationsComponent from "../components/event-destinations.js";
 
 export default class PointController {
   constructor(container) {
+    this._container = container;
 
+    this._eventComponent = null;
+    this._eventEditComponent = null;
+    this._eventOffersComponent = null;
+    this._eventDetailsComponent = new EventDetailsComponent();
+    this._eventDestinationsComponent = null;
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
 
-  render() {
+  render(eventData, event) {
+    this._eventComponent = new EventComponent(event);
+    this._eventEditComponent = new EventEditComponent(eventData, event);
+    this._eventOffersComponent = new EventOffersComponent(event);
+    this._eventDestinationsComponent = new EventDestinationsComponent(event);
 
+    this._eventComponent.setEventRollupButtonClickHandler(() => {
+      this._replaceEventToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._eventEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(
+        this._container,
+        this._eventComponent,
+        RenderPosition.BEFOREEND
+    );
+
+    render(
+        this._eventEditComponent.getElement(),
+        this._eventDetailsComponent,
+        RenderPosition.BEFOREEND
+    );
+
+    if (event.offers.length !== 0) {
+      render(
+          this._eventDetailsComponent.getElement(),
+          this._eventOffersComponent,
+          RenderPosition.BEFOREEND
+      );
+    }
+    if (event.destination.description.length || event.destination.photo.length !== 0) {
+      render(
+          this._eventDetailsComponent.getElement(),
+          this._eventDestinationsComponent,
+          RenderPosition.BEFOREEND
+      );
+    }
+  }
+
+  _replaceEventToEdit() {
+    replace(this._eventEditComponent, this._eventComponent);
+  }
+
+  _replaceEditToEvent() {
+    replace(this._eventComponent, this._eventEditComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscape = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscape) {
+      this._replaceEditToEvent();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,79 +1,12 @@
-import EventDestinationsComponent from "../components/event-destinations.js";
-import EventDetailsComponent from "../components/event-details.js";
-import EventEditComponent from "../components/event-edit.js";
-import EventComponent from "../components/event.js";
-import EventOffersComponent from "../components/event-offers.js";
 import EventSortComponent, {SortType} from "../components/event-sort.js";
 import NoPointsComponent from "../components/no-points.js";
 import TripDayComponent from "../components/trip-day.js";
 import TripDaysComponent from "../components/trip-days.js";
+import PointController from "./point.js";
 import {generateEventData} from "../mock/eventData.js";
 import {createEventGroups} from "../utils/common.js";
-import {RenderPosition, render, replace} from "../utils/render.js";
+import {RenderPosition, render} from "../utils/render.js";
 import SortDayComponent from "../components/sort-day.js";
-
-const renderEvent = (eventListElement, event) => {
-
-  const replaceEventToEdit = () => {
-    replace(eventEditComponent, eventComponent);
-  };
-
-  const replaceEditToEvent = () => {
-    replace(eventComponent, eventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscape = evt.key === `Escape` || evt.key === `Esc`;
-
-    if (isEscape) {
-      replaceEditToEvent();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const eventComponent = new EventComponent(event);
-  eventComponent.setEventRollupButtonClickHandler(() => {
-    replaceEventToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  const eventData = generateEventData();
-  const eventEditComponent = new EventEditComponent(eventData, event);
-
-  eventEditComponent.setSubmitHandler((evt) => {
-    evt.preventDefault();
-    replaceEditToEvent();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(
-      eventListElement,
-      eventComponent,
-      RenderPosition.BEFOREEND
-  );
-
-  const eventDetailsComponent = new EventDetailsComponent();
-
-  render(
-      eventEditComponent.getElement(),
-      eventDetailsComponent,
-      RenderPosition.BEFOREEND
-  );
-  if (event.offers.length !== 0) {
-    render(
-        eventDetailsComponent.getElement(),
-        new EventOffersComponent(event),
-        RenderPosition.BEFOREEND
-    );
-  }
-  if (event.destination.description.length || event.destination.photo.length !== 0) {
-    render(
-        eventDetailsComponent.getElement(),
-        new EventDestinationsComponent(event),
-        RenderPosition.BEFOREEND
-    );
-  }
-};
 
 const getSortedEvents = (events, sortType) => {
   let sortedEvents = [];
